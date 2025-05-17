c87369ebc532c58ea79d6cbc8d11089e6084db93
Fajar Riansyah <fajary781@gmail.com>
2025-05-17 20:01:42 +0700

    feat (database seeder): register all seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'),
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            CourseSeeder::class,
+            DaySeeder::class,
         ]);
     }
 }
