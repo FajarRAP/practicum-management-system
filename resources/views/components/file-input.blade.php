273ec14c1bca91478769cb919853c1682eee84cb
Fajar Riansyah <fajary781@gmail.com>
2025-05-19 14:07:36 +0700

    feat: add file input component

---
+++ resources/views/components/file-input.blade.php
@@ -0,0 +1,7 @@
+@props(['disabled' => false])
+
+<input type="file" @disabled($disabled)
+    {{ $attributes->merge([
+        'class' =>
+            'block w-full text-sm text-gray-700 border border-gray-300 rounded-md shadow-sm cursor-pointer focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 file:mr-4 file:ms-1 file:my-1 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100',
+    ]) }}>
