d6024b832f27b981ac4c02775195606100e1c39b
Fajar Riansyah <fajary781@gmail.com>
2025-05-22 23:55:33 +0700

    chore (file nput): remove width class

--- resources/views/components/file-input.blade.php
+++ resources/views/components/file-input.blade.php
@@ -3,5 +3,5 @@
 <input type="file" @disabled($disabled)
     {{ $attributes->merge([
         'class' =>
-            'block w-full text-sm text-gray-700 border border-gray-300 rounded-md shadow-sm cursor-pointer focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 file:mr-4 file:ms-1 file:my-1 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100',
+            'block text-sm text-gray-700 border border-gray-300 rounded-md shadow-sm cursor-pointer focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 file:mr-4 file:ms-1 file:my-1 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100',
     ]) }}>
