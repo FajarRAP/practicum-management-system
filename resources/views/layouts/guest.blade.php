8838a97a192317de41698d1738a30993fd65984a
Fajar Riansyah <fajary781@gmail.com>
2025-06-04 23:23:04 +0700

    feat (guest layout): add session alert components for error and success messages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,12 +19,20 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
+
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-lime-50">
         <div>
             <a href="/">
                 <img src="{{ Vite::asset('resources/images/logo.png') }}" class="h-20 fill-current text-gray-500" />
             </a>
         </div>
+
+        @session('error')
+            <x-danger-alert :$value />
+        @endsession
+        @session('success')
+            <x-success-alert :$value />
+        @endsession
 
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-slate-100 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
