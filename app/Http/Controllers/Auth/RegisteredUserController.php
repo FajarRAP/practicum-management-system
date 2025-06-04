1e8644f7895aa7b85bc1b7384a0591121c2d5cc6
Fajar Riansyah <fajary781@gmail.com>
2025-06-04 23:04:58 +0700

    feat (registration): integrate security questions into user registration process

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\SecurityQuestion;
 use App\Models\User;
+use App\Models\UserSecurityQuestion;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -22,6 +24,7 @@ class RegisteredUserController extends Controller
     public function create(): View
     {
         return view('auth.register', [
+            'security_question' => SecurityQuestion::first(),
             'roles' => Role::all()->map(fn($role) => [
                 'id' => $role->id,
                 'name' => $role->name,
@@ -41,19 +44,35 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'user_role' => ['required'],
+            'security_question' => ['required', 'exists:security_questions,id'],
+            'security_question_answer' => ['required', 'string', 'max:255'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-        ])->assignRole($request->user_role);
+        ])->assignRole($this->evaluateEmail($request->email));
+
+        UserSecurityQuestion::create([
+            'user_id' => $user->id,
+            'security_question_id' => $request->security_question,
+            'answer' => $request->security_question_answer,
+        ]);
 
         event(new Registered($user));
 
         Auth::login($user);
 
         return redirect(route('dashboard', absolute: false));
+    }
+    private function evaluateEmail(string $email): string
+    {
+        if (Str::of($email)->endsWith('@asist.trisakti.ac.id')) return 'assistant';
+        if (Str::of($email)->endsWith('@std.trisakti.ac.id')) return 'student';
+        if (Str::of($email)->endsWith('@trisakti.ac.id')) return 'lecturer';
+        if (Str::of($email)->endsWith('@laboran.trisakti.ac.id')) return 'lab_tech';
+
+        return 'student';
     }
 }
