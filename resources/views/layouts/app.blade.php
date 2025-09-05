7934907e759fdd071455b880f0b23edb7f6efed6
Fajar Riansyah <fajary781@gmail.com>
2025-09-06 01:04:18 +0700

    feat (app layout): enhance header layout with active academic year display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,14 +25,32 @@
         <!-- Page Heading -->
         @isset($header)
             <header class="bg-slate-100 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
                     {{ $header }}
+
+                    @if (isset($activeAcademicYear))
+                        <div class="text-sm">
+                            <span class="font-medium text-gray-500">Tahun Ajaran Aktif:</span>
+                            <span
+                                class="ms-2 inline-flex items-center px-3 py-1 rounded-full text-xs font-semibold bg-green-100 text-green-800">
+                                {{ $activeAcademicYear->year }} - {{ $activeAcademicYear->semester }}
+                            </span>
+                        </div>
+                    @else
+                        <div class="text-sm">
+                            <span
+                                class="ms-2 inline-flex items-center px-3 py-1 rounded-full text-xs font-semibold bg-yellow-100 text-yellow-800">
+                                Tidak ada tahun ajaran yang aktif
+                            </span>
+                        </div>
+                    @endif
                 </div>
+
             </header>
         @endisset
 
         <!-- Page Content -->
-        <main>
+        <main {{ $attributes }}>
             @session('error')
                 <x-danger-alert :$value />
             @endsession
