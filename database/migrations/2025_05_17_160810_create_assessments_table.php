8ef4a57fe34da7fa7de5761faf235c963b26daff
Fajar Riansyah <fajary781@gmail.com>
2025-09-09 21:14:29 +0700

    fix (migration): update foreign key and unique constraint in assessments table

--- database/migrations/2025_05_17_160810_create_assessments_table.php
+++ database/migrations/2025_05_17_160810_create_assessments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('assessments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('announcement_id')
+            $table->foreignId('schedule_id')
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->float('report_score')->nullable();
             $table->float('active_score')->nullable();
             $table->float('module_score')->nullable();
-            $table->unique(['announcement_id', 'user_id']);
+            $table->unique(['schedule_id', 'user_id']);
             $table->timestamps();
         });
     }
