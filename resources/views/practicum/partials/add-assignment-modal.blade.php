63ec26124b433e4a2c3d2c595ac6c2bb3cd375af
Fajar Riansyah <fajary781@gmail.com>
2025-09-09 21:11:56 +0700

    feat: add add assignment modal partial

---
+++ resources/views/practicum/partials/add-assignment-modal.blade.php
@@ -0,0 +1,37 @@
+<x-modal name="add-assignment-modal" :show="$errors->addAssignment->isNotEmpty()" focusable>
+    <form method="POST" action="{{ route('assignment.store') }}" class="p-6" enctype="multipart/form-data">
+        @csrf
+        <input type="hidden" name="practicum_id" value="{{ $practicum->id }}">
+
+        <h2 class="text-lg font-medium text-gray-900">{{ __('Create New Assignment') }}</h2>
+        <div class="mt-6 space-y-4">
+            <div>
+                <x-input-label for="title" value="{{ __('Title') }}" />
+                <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" :value="old('title')"
+                    required />
+                <x-input-error :messages="$errors->addAssignment->get('title')" class="mt-2" />
+            </div>
+            <div>
+                <x-input-label for="description" value="{{ __('Description') }}" />
+                <x-textarea id="description" name="description"
+                    class="mt-1 block w-full">{{ old('description') }}</x-textarea>
+                <x-input-error :messages="$errors->addAssignment->get('description')" class="mt-2" />
+            </div>
+            <div>
+                <x-input-label for="deadline" value="{{ __('Deadline') }}" />
+                <x-text-input id="deadline" name="deadline" type="datetime-local" class="mt-1 block w-full"
+                    :value="old('deadline')" required />
+                <x-input-error :messages="$errors->addAssignment->get('deadline')" class="mt-2" />
+            </div>
+            <div>
+                <x-input-label for="file" value="{{ __('Attachment (Optional)') }}" />
+                <x-file-input id="file" name="file" class="mt-1 block w-full" />
+                <x-input-error :messages="$errors->addAssignment->get('file')" class="mt-2" />
+            </div>
+        </div>
+        <div class="mt-6 flex justify-end">
+            <x-secondary-button x-on:click="$dispatch('close')">{{ __('Cancel') }}</x-secondary-button>
+            <x-primary-button class="ms-3">{{ __('Create Assignment') }}</x-primary-button>
+        </div>
+    </form>
+</x-modal>
