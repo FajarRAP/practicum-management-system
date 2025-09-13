1365b67b01da4cc6009cf3e9dba87b8ef8510c5e
Fajar Riansyah <fajary781@gmail.com>
2025-09-13 20:22:47 +0700

    fix: add missing id column to schedules table migration

--- database/migrations/2025_05_17_132729_create_schedules_table.php
+++ database/migrations/2025_05_17_132729_create_schedules_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('practicum_id')
                 ->constrained()
                 ->onUpdate('cascade')
