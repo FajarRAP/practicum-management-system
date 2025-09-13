0ced45504382b8588d41ae2c626302912c39190f
Fajar Riansyah <fajary781@gmail.com>
2025-09-13 20:23:01 +0700

    refactor: reorder methods in Schedule model for clarity and maintainability

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,33 +8,18 @@ class Schedule extends Model
 {
     protected $guarded = [];
 
-    // public function course()
-    // {
-    //     return $this->belongsTo(Course::class);
-    // }
-
-    // public function day()
-    // {
-    //     return $this->belongsTo(Day::class);
-    // }
-
-    // public function announcements()
-    // {
-    //     return $this->hasMany(Announcement::class);
-    // }
-
-    // public function enrollments()
-    // {
-    //     return $this->hasMany(Enrollment::class);
-    // }
-
-    public function practicum()
+    public function assignment()
     {
-        return $this->belongsTo(Practicum::class);
+        return $this->hasOne(Assignment::class);
     }
 
     public function attendances()
     {
         return $this->hasMany(Attendance::class);
+    }
+
+    public function practicum()
+    {
+        return $this->belongsTo(Practicum::class);
     }
 }
