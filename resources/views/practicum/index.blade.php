a4aac2f636eb769eaf17fcf05e11000306106a26
Fajar Riansyah <fajary781@gmail.com>
2025-09-14 22:12:52 +0700

    feat (practicum management): update access control for practicum actions using permissions

--- resources/views/practicum/index.blade.php
+++ resources/views/practicum/index.blade.php
@@ -7,11 +7,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-4">
-            @hasrole('assistant')
-                <x-primary-button class="self-end" x-data @click="$dispatch('open-modal', 'add-practicum')">
+            @can('practicum.add')
+                <x-primary-button class="self-end" x-data x-on:click="$dispatch('open-modal', 'add-practicum')">
                     {{ __('Add Practicum') }}
                 </x-primary-button>
-            @endhasrole
+            @endcan
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg min-h-96 flex flex-col justify-between">
@@ -66,12 +66,14 @@
                                                 class="px-2 py-1 font-semibold leading-tight text-yellow-700 bg-yellow-100 rounded-full text-xs">{{ __('Draft') }}</span>
                                         @endif
                                     </td>
-                                    <td class="py-4 px-6 flex items-center justify-start space-x-4">
-                                        @hasrole('assistant')
+                                    <td class="py-4 px-6">
+                                        @can('practicum.view')
                                             <a href="{{ route('practicum.show', $practicum) }}"
                                                 class="font-medium text-indigo-600 hover:underline">
-                                                {{ __('Manage') }}
+                                                {{ __('View') }}
                                             </a>
+                                        @endcan
+                                        @can('practicum.delete')
                                             <form action="{{ route('practicum.destroy', $practicum) }}" method="POST"
                                                 onsubmit="return confirm('Are you sure you want to delete this practicum?');">
                                                 @csrf
@@ -80,13 +82,7 @@
                                                     {{ __('Delete') }}
                                                 </button>
                                             </form>
-                                        @endhasrole
-                                        @hasrole('lab_tech')
-                                            <a href="{{ route('practicum.show', $practicum) }}"
-                                                class="font-medium text-indigo-600 hover:underline">
-                                                {{ __('View') }}
-                                            </a>
-                                        @endhasrole
+                                        @endcan
                                     </td>
                                 </tr>
                             @empty
@@ -105,5 +101,7 @@
         </div>
     </div>
 
-    @include('practicum.partials.add-practicum-modal')
+    @can('practicum.add')
+        @include('practicum.partials.add-practicum-modal')
+    @endcan
 </x-app-layout>
