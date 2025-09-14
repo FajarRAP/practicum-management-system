e784f7690400240f0dded4ce7c20680e984dada5
Fajar Riansyah <fajary781@gmail.com>
2025-09-14 22:10:26 +0700

    refactor (RoleSeeder): update roles and permissions, adding new permissions for students, assistants, lecturers, and lab techs

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
@@ -13,9 +14,114 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $roles = ['student', 'assistant', 'lecturer', 'lab_tech', 'practicum_chief', 'lab_chief'];
-        foreach ($roles as $role) {
-            Role::create(['name' => $role]);
+        $roles = ['student', 'assistant', 'lecturer', 'programming_lecturer', 'isad_lecturer', 'lab_tech', 'super_admin'];
+        $viewPermissions = [
+            'academic_year.view',
+            'archive.view',
+            'assignment.view',
+            'practicum.view',
+            'shift.view',
+            'student_datas.view',
+            'students.view',
+            'submission.view',
+        ];
+        $permissions = [
+            // assistant
+            'academic_year.view',
+            'archive.view',
+            'assignment.view',
+            'practicum.view',
+            'shift.view',
+            'student_datas.view',
+            'students.view',
+            'submission.view',
+            'academic_year.add',
+            'academic_year.edit',
+            'academic_year.delete',
+            'shift.add',
+            'shift.edit',
+            'shift.delete',
+            'practicum.add',
+            'practicum.delete',
+            'archive.add',
+            'archive.delete',
+            'schedule.add',
+            'schedule.edit',
+            'schedule.delete',
+            'manage_journal',
+            'assignment.add',
+            'assignment.edit',
+            'assignment.delete',
+            'scores.calculate',
+            // lab tech
+            'schedule.approve',
+            // student
+            'practicum.enroll',
+            'practicum.enter',
+            // lecturer
+            'teach_isad',
+            'teach_programming',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
         }
+
+        Role::create(['name' => 'student'])->givePermissionTo([
+            'practicum.enroll',
+            'practicum.enter',
+        ]);
+
+        Role::create(['name' => 'assistant'])->givePermissionTo([
+            'academic_year.add',
+            'academic_year.edit',
+            'academic_year.delete',
+            'academic_year.view',
+            'shift.add',
+            'shift.edit',
+            'shift.delete',
+            'shift.view',
+            'practicum.add',
+            'practicum.delete',
+            'practicum.view',
+            'archive.add',
+            'archive.delete',
+            'archive.view',
+            'students.view',
+            'student_datas.view',
+            'schedule.add',
+            'schedule.edit',
+            'schedule.delete',
+            'manage_journal',
+            'assignment.add',
+            'assignment.edit',
+            'assignment.delete',
+            'assignment.view',
+            'submission.view',
+            'scores.calculate',
+        ]);
+
+        Role::create(['name' => 'lecturer'])->givePermissionTo([
+            'teach_isad',
+            'teach_programming',
+            ...$viewPermissions,
+        ]);
+
+        Role::create(['name' => 'programming_lecturer'])->givePermissionTo([
+            'teach_programming',
+            ...$viewPermissions,
+        ]);
+
+        Role::create(['name' => 'isad_lecturer'])->givePermissionTo([
+            'teach_isad',
+            ...$viewPermissions,
+        ]);
+
+        Role::create(['name' => 'lab_tech'])->givePermissionTo([
+            'schedule.approve',
+            ...$viewPermissions,
+        ]);
+
+        Role::create(['name' => 'super_admin']);
     }
 }
